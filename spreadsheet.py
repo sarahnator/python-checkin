0521266703779be25e026fb1bb1d59a5904e26b6
Sarah Etter <sarahett@usc.edu>
2020-05-18 22:34:53 -0400

    add json files

--- spreadsheet.py
+++ spreadsheet.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('../client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('../json/client_secret.json', scope)
 client = gspread.authorize(creds)
 
 # Find workbook by name and open the first sheet
