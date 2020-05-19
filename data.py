87ff625a805c2feefe0a8fac7e87b7b483b276d2
Sarah Etter <sarahett@usc.edu>
2020-05-19 13:25:06 -0400

    parsed ordered dict vals from weight

--- data.py
+++ data.py
@@ -1,11 +1,16 @@
 import myfitnesspal as pal
 import datetime
 from datetime import date
-
+import json
+from collections import OrderedDict
 if __name__ == "__main__":
     print("hello")
 
-client = pal.Client('almundbutter@gmail.com')
+# extract json information @sheetName
+with open('../json/creds.json') as src:
+    data = json.load(src)
+
+client = pal.Client(data['email'])
 
 # get system date in string format YYYYMMDD
 date = date.today()
@@ -26,21 +31,36 @@ day = client.get_date(int(y), int(m), int(d))
 
 # get pal day totals
 total = day.totals
+print(total)
+
 # int day totals
-cal = total['calories']
-pro = total['protein']
-car = total['carbohydrates']
-fat = total['fat']
-fiber = total['fiber']
+cal, pro, car, fat, fiber = 0, 0, 0, 0, 0
+
+# check if data exists
+if total:
+    cal = total['calories']
+    pro = total['protein']
+    car = total['carbohydrates']
+    fat = total['fat']
+    fiber = total['fiber']
+
 
 # check values
-# print("cal: " + str(cal) + " pro: " + str(pro) + " car: " + str(car) + " fat: " + str(fat) + " fiber " + str(fiber))
+print("cal: " + str(cal) + " pro: " + str(pro) + " car: " + str(car) + " fat: " + str(fat) + " fiber " + str(fiber))
 
-# get weights
-# since given day
+# get weights since given day
 day = datetime.date(int(y), int(m), int(d))
-weight = client.get_measurements('Weight', day)
-print(weight)
+weights = client.get_measurements('Weight', day)
+# convert ordered dictionary to list
+weights = list(weights.items())
+# extract date for 0th item format mm-dd
+weight_date = str(weights[0][0])[5:]
+print(weight_date)
+
+# extract weight on 0th day
+weight_w = weights[0][1]
+print(weight_w)
+
 
 # # behavior for undefined future weights not yet logged
 # # returns last logged weight
@@ -54,5 +74,3 @@ print(weight)
 # thiswk = datetime.date(int(y), int(m), int(d))
 # lastwk = datetime.date(int(y), int(m), int(d))
 # weight = client.get_measurements('Weight', thiswk, lastwk)
-
-# TODO: parse ordered dictionary return param for weight
