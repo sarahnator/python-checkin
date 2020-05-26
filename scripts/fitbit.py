5e8b0f20343422e2e7a61a0c7bcbd5119d016640
Sarah Etter <sarahett@usc.edu>
2020-05-25 23:59:20 -0400

    fitbit notes

--- scripts/fitbit.py
+++ scripts/fitbit.py
@@ -6,10 +6,12 @@ import json
 
 
 def init_bit_client():
+    #get credentials from json file
     with open('json/creds.json') as src:
         data = json.load(src)
     CLIENT_ID = data['fitbit-clientID']
     CLIENT_SECRET = data['fitbit-secret']
+
     #use client id and secret to get access and refresh tokens that authorize access to the data
     server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
     server.browser_authorize()
@@ -19,3 +21,16 @@ def init_bit_client():
     auth2_client = bit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
 
     print('auth success!!!!')
+
+#steps and distance query
+def query_fitbit():
+    # resource URL
+    # GET / 1 / user / [user - id] / [resource - path] / date / [base - date] / [end - date].json
+    #resource path opts:
+        # activities / tracker / steps
+        #activities/tracker/distance
+        #example request:
+    # GET https: // api.fitbit.com / 1 / user / - / activities / steps / date / today / 1m.json
+    """https://dev.fitbit.com/build/reference/web-api/activity/"""
+
+
