329b20b9977d01d3e2bc5850feceb0e17ae75c12
Sarah Etter <sarahett@usc.edu>
2020-05-26 23:22:20 -0400

    no page redirectsgit add .

--- scripts/fitbit.py
+++ scripts/fitbit.py
@@ -13,14 +13,17 @@ def fitbit_query():
         data = json.load(src)
     CLIENT_ID = data['fitbit-clientID']
     CLIENT_SECRET = data['fitbit-secret']
+    ACCESS_TOKEN = data['fitbit-token']
+    REFRESH_TOKEN = data['fitbit-refresh-token']
 
     
     #use client id and secret to get access and refresh tokens that authorize access to the data
     server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
-    server.browser_authorize()
+    # server.browser_authorize()
 
-    ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
-    REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+    
+    # ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+    # REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
     auth2_client = bit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
    
     # get end and base date for api call
